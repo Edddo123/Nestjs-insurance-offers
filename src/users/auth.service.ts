f1fb8c7ccd5ccc8be16a8e4f9aadaaf9406b14b1
Edddo123 <edo.miraqian@gmail.com>
2021-11-12 02:39:05 +0400

    hashing passwords/adding interceptor to strip out password from response

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UsersService } from './users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
   async createClient(client: CreateClientDto) {
     const { email, password } = client;
-
-    return this.usersService.createClient(email, password);
+    const hashedPassword = await bcrypt.hash(password, 12);
+    return this.usersService.createClient(email, hashedPassword);
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ClientDto } from './dto/client.dto';
 import { CreateClientDto } from './dto/create-client.dto';
+import { SerializeInterceptor } from './interceptors/serialize-user.interceptor';
 
 @Controller('users')
+@UseInterceptors(new SerializeInterceptor(ClientDto))
 export class UsersController {
   constructor(private authService: AuthService) {}
 
