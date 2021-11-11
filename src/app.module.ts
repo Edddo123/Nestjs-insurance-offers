c7c40b04d900c8a8def7de34f15165faaeae0d73
Edddo123 <edo.miraqian@gmail.com>
2021-11-12 01:55:24 +0400

    setting up typeorm connection/postgres service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, 
+    ConfigModule.forRoot({ envFilePath: `.env` }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'insurance',
+      host: 'localhost',
+      username: 'postgres',
+      port: 5432,
+      password: process.env.DB_PASSWORD
+
+    })
+  ],
   controllers: [AppController],
   providers: [
     AppService,
